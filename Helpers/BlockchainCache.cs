18fb5e7977e2cfc27d4ea250b0533d99a7f44e5b
acedais <43746008+acedais@users.noreply.github.com>
2018-10-28 15:38:56 +0530

    Update BlockchainCache.cs

--- Helpers/BlockchainCache.cs
+++ Helpers/BlockchainCache.cs
@@ -109,7 +109,7 @@ namespace WebWallet.Helpers
                 {
                     currentHeight = 0;
                 }
-                var startHeight = 0;
+                var startHeight = 1;
                 var endHeight = Convert.ToInt32(Math.Ceiling((double)(currentHeight / 10000) * 10000)) + 10000;
                 logger.Log(LogLevel.Information, $"Processing transactions from blocks {startHeight} to {endHeight}");
                 //now, splt the current height into blocks of 10000
@@ -151,15 +151,10 @@ namespace WebWallet.Helpers
                         {
                             LogException(ex);
                         }
-                        var counter = 0;
+                        var counter = 1;
                         var gCounter = start;
 
                         var txHashes = new List<string>();
-                        
-                        if (txHashes == null)
-                        {
-                            return;
-                        }
 
                         if (currentHeight > start)
                         {
