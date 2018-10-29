e8c968a6aabca6b37b3c860e58699487b1b62326
Mayilvahanan Arumugam <mayilvahanan@Mayilvahanans-MacBook-Pro.local>
2018-10-29 08:11:51 +0530

    Update

--- wwwroot/config.ts
+++ wwwroot/config.ts
@@ -1,28 +1,27 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
     apiUrl: typeof window !== 'undefined' && window.location ? window.location.href.substr(0, window.location.href.lastIndexOf('/') + 1) + 'api/' : 'https://getkaasu.com/api/',
-    mainnetExplorerUrl: "https://getkaasu.info",
-    testnetExplorerUrl: "https://getkaasu.info/",
-    testnet: false,
+	mainnetExplorerUrl: "https://getkaasu.info",
+	testnetExplorerUrl: "https://getkaasu.info/",
+	testnet: false,
     coinUnitPlaces: 5,
-    txMinConfirms: 60,         // corresponds to CRYPTONOTE_DEFAULT_TX_SPENDABLE_AGE in Monero
-    txCoinbaseMinConfirms: 160, // corresponds to CRYPTONOTE_MINED_MONEY_UNLOCK_WINDOW in Monero
-    addressPrefix: 111,
-    integratedAddressPrefix: 111,
-    addressPrefixTestnet: 0,
-    integratedAddressPrefixTestnet: 0,
-    subAddressPrefix: 0,
-    subAddressPrefixTestnet: 0,
-    feePerKB: new JSBigInt('10'),//20^10 - for testnet its not used, as fee is dynamic.
-    dustThreshold: new JSBigInt('1'),//10^10 used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
-    defaultMixin: 0, // default value mixin
-    idleTimeout: 30,
-    idleWarningDuration: 20,
-
-    coinSymbol: 'KSU',
-    openAliasPrefix: "ksu",
-    coinName: 'KAASU',
-    coinUriPrefix: 'kaasu:',
-    avgBlockTime: 10,
-    maxBlockNumber: 500000000,
+    coinDisplayUnitPlaces: 5,
+	txMinConfirms: 60,
+	txCoinbaseMinConfirms: 160,
+	addressPrefix: 111,
+	integratedAddressPrefix: 111,
+	addressPrefixTestnet: 0,
+	integratedAddressPrefixTestnet: 0,
+	subAddressPrefix: 0,
+	subAddressPrefixTestnet: 0,
+	feePerKB: new JSBigInt('10'), //for testnet its not used, as fee is dynamic.
+	dustThreshold: new JSBigInt('10'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	defaultMixin: 0, // default value mixins
+	idleTimeout: 30,
+	idleWarningDuration: 20,
+	coinSymbol: 'KSU',
+	coinName: 'Kaasu',
+	coinUriPrefix: 'kaasu:',
+	avgBlockTime: 120,
+	maxBlockNumber: 500000000,
 };
