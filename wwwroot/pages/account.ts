66e099d0d2332c0d3cc914f65ab10b31d7e405d1
Mayilvahanan Arumugam <mayilvahanan@Mayilvahanans-MacBook-Pro.local>
2018-11-16 16:07:15 +0530

    update

--- wwwroot/pages/account.ts
+++ wwwroot/pages/account.ts
@@ -35,7 +35,7 @@ class AccountView extends DestructableView{
 
 	@VueVar(0) currentScanBlock !: number;
 	@VueVar(0) blockchainHeight !: number;
-	@VueVar(100) currencyDivider !: number;
+	@VueVar(100000) currencyDivider !: number;
 
     intervalRefresh : number = 0;
 
